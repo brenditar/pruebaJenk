cb4a0d444cb879107fdb221e88faa17a3e0d2db8
Brenda Rodriguez <brodriguez@engee.local>
2016-11-24 15:18:44 -0300

    test in pruebaJenkins

--- src/CX/pruebaJenkins.java
+++ src/CX/pruebaJenkins.java
@@ -3,12 +3,27 @@ package CX;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class pruebaJenkins {
+	
+	static WebDriver driver = new FirefoxDriver();
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		abrir();
+		cerrar();
 	}
 
+	
+	public static void abrir(){
+		// Abrimos el sitio
+		driver.navigate().to("http://www.google.com.ar");
+		driver.manage().window().maximize();
+	}
+	
+	public static void cerrar(){
+		driver.quit();
+	}
 }
